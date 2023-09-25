b03b44caa4e6bc56ea78a265191983c9e646f62d
Michael Feher <michael.feher@algorand.foundation>
2023-09-25 15:34:43 -0500

    fix: origin and hostname for vercel

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,24 @@
+const isProduction = process.env.VERCEL_ENV === 'production';
+const isVercel = typeof process.env.VERCEL === 'string';
+
+const hostname = isVercel
+  ? isProduction
+    ? 'nest-fido2.vercel.app'
+    : process.env.VERCEL_URL.replace('https://', '')
+  : 'localhost';
+
+const origin = isProduction
+  ? 'https://' + hostname
+  : isVercel
+  ? process.env.VERCEL_URL
+  : 'http://localhost';
+
 export default () => ({
   env: process.env.NODE_ENV || 'development',
   timeout: 30 * 1000 * 60,
   rpName: process.env.RP_NAME || 'Algorand Foundation FIDO2 Server',
-  hostname: process.env.HOSTNAME || process.env.VERCEL_URL || 'localhost',
-  origin: process.env.ORIGIN || 'http://localhost',
-  //TODO: Android App
+  hostname,
+  origin,
   android: { hash: process.env.ANDROID_SHA256HASH || '' },
   database: {
     host: process.env.DB_HOST || 'localhost:27017',
