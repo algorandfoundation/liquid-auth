9ec8802d88b7b0aa4c580c3c97f7a47b9e00721a
Michael J Feher <github@phearzero.com>
2023-09-25 19:59:39 -0500

    feat: add ua-parser

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import base64url from 'base64url';
+import UAParser from 'ua-parser-js';
 
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) {}
   getOrigin(ua: string): string {
     let origin: string;
+    const parser = new UAParser(ua);
     // Android origin
-    if (ua.indexOf('okhttp') === 0) {
+    if (parser.getOS().name.includes('Android')) {
       // TODO: Ensure we are encoding properly for android
       const octArray: unknown = this.configService
         .get<{ hash: string }>('android')
