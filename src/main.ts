f8701c7f5393923e98e501dac409f144a3c62a2b
Michael Feher <michael.feher@algorand.foundation>
2023-10-18 12:00:03 -0500

    feat: add sentry monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { resolve } from 'node:path';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import session from 'express-session';
 import hbs from 'hbs';
@@ -8,10 +8,28 @@ import MongoStore from 'connect-mongo';
 import { AppModule } from './app.module.js';
 import { RedisIoAdapter } from './adapters/redis-io.adapter.js';
 import { ConfigService } from '@nestjs/config';
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
+import { SentryFilter } from './sentry.filter.js';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+
   const config = app.get<ConfigService>(ConfigService);
   const env = config.get('env');
 
