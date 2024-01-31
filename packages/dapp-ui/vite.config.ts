09d2b7cd5c9b20481712817870caeb903dc16026
Michael J Feher <github@phearzero.com>
2024-01-31 17:54:04 -0600

    debug: onrender deployment

--- packages/dapp-ui/vite.config.ts
+++ packages/dapp-ui/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import {rename} from 'node:fs/promises'
+import {rename, readdir} from 'node:fs/promises'
 import {resolve} from 'node:path'
 import { mkdirp } from 'mkdirp';
 // https://vitejs.dev/config/
 
-const API_DIR = resolve(__dirname, '../avicennia-api')
+const API_DIR = resolve(__dirname, '..', 'avicennia-api')
 const PUBLIC_DIR = resolve(API_DIR, 'public')
 const VIEW_DIR = resolve(API_DIR, 'views')
 
@@ -13,12 +13,15 @@ export default defineConfig({
     build:{
       outDir: PUBLIC_DIR,
     },
-    plugins: [react(),   {
-      name: 'move-index-file',
-      closeBundle: async () => {
-        await mkdirp(PUBLIC_DIR)
-        await mkdirp(VIEW_DIR)
-        await rename(resolve(PUBLIC_DIR, 'index.html'), resolve(VIEW_DIR, 'index.html'))
-      }
-    },],
+    plugins: [react(),
+      // {
+      // name: 'move-index-file',
+      // closeBundle: async () => {
+      //   await mkdirp(VIEW_DIR)
+      //   console.log(await readdir(API_DIR))
+      //   console.log(await readdir(PUBLIC_DIR))
+      //   await rename(resolve(PUBLIC_DIR, 'index.html'), resolve(VIEW_DIR, 'index.html'))
+      // }
+    // },
+    ],
 })
