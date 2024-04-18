287df39b6966161bb93b5327eadfd95536f143d9
Kyle Breeding <kyle.breeding@algorand.foundation>
2024-04-18 15:57:08 -0700

    wrapped up hero section landing aesthetics

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -1,11 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+      },
+      fontSize: {
+        '8xl': ['5.25rem', { lineHeight: '1' }]
+      },
       colors: {
         'liquid-purple': '#9966FF',
-        'liquid-blue': '#906FFF'
+        'liquid-blue': '#6699FF',
+        'liquid-alt-purple': '#906FFF',
+        'liquid-green': '#00773a'
+      },
+      spacing: {
+        100: '25rem',
+        108: '27rem',
+        116: '29rem',
+        120: '30rem'
       },
     },
 	},
