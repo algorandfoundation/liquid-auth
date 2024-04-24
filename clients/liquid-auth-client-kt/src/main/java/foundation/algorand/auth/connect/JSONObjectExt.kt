9fe39aee553fb2ecfb35c2963f5f7f3992a97274
Michael Feher <michael.feher@algorand.foundation>
2024-04-24 12:51:15 -0400

    chore: remove kotlin client

--- clients/liquid-auth-client-kt/src/main/java/foundation/algorand/auth/connect/JSONObjectExt.kt
+++
@@ -1,8 +0,0 @@
-package foundation.algorand.auth.connect
-
-import org.json.JSONObject
-import org.webrtc.IceCandidate
-
-fun JSONObject.toIceCandidate(): IceCandidate {
-    return IceCandidate(getString("sdpMid"), getInt("sdpMLineIndex"), getString("candidate"))
-}
