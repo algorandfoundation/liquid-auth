868192e210ce19cd93944ed06a182691afb836fa
Michael Feher <michael.feher@algorand.foundation>
2024-04-27 00:39:41 -0400

    test: remove skipped tests

--- services/liquid-auth-api-js/src/app.module.spec.ts
+++ services/liquid-auth-api-js/src/app.module.spec.ts
@@ -1,24 +1,9 @@
-import { AppModule, mongooseModuleFactory } from './app.module.js';
-import { Test } from '@nestjs/testing';
-import { SignalsGateway } from './signals/signals.gateway.js';
-import { AttestationModule } from './attestation/attestation.module.js';
-import { AssertionModule } from './assertion/assertion.module.js';
+import { mongooseModuleFactory } from './app.module.js';
 
 import databaseConfigFixtures from './__fixtures__/configuration.database.fixtures.json';
 import { ConfigService } from '@nestjs/config';
 
 describe('AppModule', () => {
-  it.skip('should create the module', async () => {
-    //TODO: Implement in-memory database for testing
-    const module = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    expect(module).toBeDefined();
-    expect(module.get(SignalsGateway)).toBeInstanceOf(SignalsGateway);
-    expect(module.get(AttestationModule)).toBeInstanceOf(AttestationModule);
-    expect(module.get(AssertionModule)).toBeInstanceOf(AssertionModule);
-  });
   it('should create the module with atlas configuration', async () => {
     databaseConfigFixtures.forEach((fixture) => {
       const mockConfigService = {
