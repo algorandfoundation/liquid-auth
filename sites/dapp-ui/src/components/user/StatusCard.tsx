bc99fcfe0d7453863ef5be5688028544f1c65316
Kyle Breeding <kyle.breeding@algorand.foundation>
2024-06-18 12:53:51 -0700

    chore(dapp-ui): removing unnecessary changes

--- sites/dapp-ui/src/components/user/StatusCard.tsx
+++ sites/dapp-ui/src/components/user/StatusCard.tsx
@@ -33,9 +33,6 @@ export type ProfileCardProps = {
 export function StatusCard({ session, user, socket }: ProfileCardProps) {
   const { refetch } = useUserState();
   const { client, setDataChannel } = useContext(SignalClientContext);
-
-  const isLoading = socket.isConnected && !socket.hasDataChannel;
-
   return (
     <Card sx={{ maxWidth: 300, zIndex: 1000 }} raised>
       <CardContent>
@@ -84,13 +81,6 @@ export function StatusCard({ session, user, socket }: ProfileCardProps) {
           <IconButton aria-label="share">
             <ShareIcon />
           </IconButton>
-          {
-            isLoading && (
-              <Typography variant="h6" color="text.secondary">
-                Loading...
-              </Typography>
-            )
-          }
         </CardActions>
       )}
     </Card>
