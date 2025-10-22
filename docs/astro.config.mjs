a9977b5696ec4f028ae1a6eab7f8097567c35e2d
Larkin Young <larkin.young@algorand.foundation>
2025-10-22 08:36:46 +0100

    Upgraded astro, starlight, and tailwind packages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import rehypeMermaid from 'rehype-mermaid';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
 // import starlightTypeDoc, { typeDocSidebarGroup } from "starlight-typedoc";
 import react from '@astrojs/react';
@@ -11,7 +11,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [basicSsl()],
+    plugins: [basicSsl(), tailwindcss()],
     server: {
       https: true,
     },
@@ -24,7 +24,7 @@ export default defineConfig({
     starlight({
       title: 'Liquid Auth',
       favicon: './public/logo.svg',
-      customCss: ['./src/styles/mermaid.css'],
+      customCss: ['./src/styles/mermaid.css', './src/styles/global.css'],
       logo: {
         src: './public/logo.svg',
       },
@@ -242,7 +242,6 @@ export default defineConfig({
         },
       ],
     }),
-    tailwind(),
     react(),
     mdx(),
   ],
