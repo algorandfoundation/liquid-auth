fad6d7fd43cf78ee6b240de2ca510272ca565088
Michael J Feher <michael.feher@algorand.foundation>
2026-03-09 11:38:10 -0400

    chore(view): add index view for testing

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Render } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'TODO: inject variables into service' };
+  }
+}
